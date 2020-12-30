4bd3e1e2964a03f0c199c819cfd96e5928eca3be
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2020-12-30 14:25:09 +0100

    [BAR-5]Ajout du score

--- Core/src/main/java/com/barabieres/game/Game.java
+++ Core/src/main/java/com/barabieres/game/Game.java
@@ -2,4 +2,48 @@ package com.barabieres.game;
 
 public class Game {
 
+    private double tresorery;
+    private int score;
+    private int winAt;
+
+    public Game(double tresorery, int winAt) {
+        this.tresorery = tresorery;
+        this.winAt = winAt;
+        this.score = 0;
+    }
+
+    public double getTresorery() {
+        return tresorery;
+    }
+
+    //augmente la trésorerie du nombre rentré en paramètre
+    public void increaseTresorery(double nbAdd) {
+        this.setTresorery(this.getTresorery() + nbAdd);
+    }
+
+    //diminue la trésorerie du nombre rentré en paramètre
+    public void decreaseTresorery(double nbDel) {
+        this.setTresorery(this.getTresorery() - nbDel);
+    }
+
+    public void setTresorery(double tresorery) {
+        this.tresorery = tresorery;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    //augmente le score du nombre de points passés en paramètres
+    public void inscreaseScore(int nbPointsAdd) {
+        this.setScore(this.getScore() + nbPointsAdd);
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getWinAt() {
+        return this.winAt;
+    }
 }
--- Core/src/test/java/com/barabieres/game/GameTest.java
+++ Core/src/test/java/com/barabieres/game/GameTest.java
@@ -1,5 +1,63 @@
 package com.barabieres.game;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GameTest {
+
+    Game game = new Game(2000, 300);
+
+    @Test
+    public void should_get_tresorery() {
+
+        assertEquals(game.getTresorery(), 2000);
+    }
+
+    @Test
+    public void should_set_tresorery() {
+        game.setTresorery(1000);
+
+        assertEquals(game.getTresorery(), 1000);
+    }
+
+    @Test
+    public void should_increase_tresorery() {
+        game.increaseTresorery(10);
+
+        assertEquals(game.getTresorery(), 2010);
+    }
+
+    @Test
+    public void should_decrease_tresorery() {
+        game.decreaseTresorery(10);
+
+        assertEquals(game.getTresorery(), 1990);
+    }
+
+    @Test
+    public void should_get_score() {
+        assertEquals(game.getScore(), 0);
+    }
+
+    @Test
+    public void should_set_score() {
+        game.setScore(100);
+
+        assertEquals(game.getScore(), 100);
+    }
+
+    @Test
+    public void should_increase_score() {
+        game.inscreaseScore(150);
+
+        assertEquals(game.getScore(), 150);
+    }
+
+    @Test
+    public void should_get_win_at() {
+        assertEquals(game.getWinAt(), 300);
+    }
 
 }
