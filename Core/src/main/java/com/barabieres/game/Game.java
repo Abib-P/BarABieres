a9d792e74c462f414fc9a65f450f52ec70935a91
PatateHunter <ichtitski@gmail.com>
2020-12-30 19:46:23 +0100

    Ichai  classe Inventory,Game CashFlow

--- Core/src/main/java/com/barabieres/game/Game.java
+++ Core/src/main/java/com/barabieres/game/Game.java
@@ -1,5 +1,21 @@
 package com.barabieres.game;
 
+import com.barabieres.cashflow.CashFlow;
+
 public class Game {
+    private CashFlow goalAmount;
+
+    public Game() {
+        this.goalAmount = new CashFlow(0.00);
+    }
+    public Game(CashFlow goalAmount) {
+        this.goalAmount = goalAmount;
+    }
+    public CashFlow getGoalAmount() {
+        return goalAmount;
+    }
+    public void setGoalAmount(CashFlow goalAmount) {
+        this.goalAmount = goalAmount;
+    }
 
 }
--- Core/src/test/java/com/barabieres/game/GameTest.java
+++ Core/src/test/java/com/barabieres/game/GameTest.java
@@ -1,5 +1,23 @@
 package com.barabieres.game;
+import com.barabieres.cashflow.CashFlow;
+import com.barabieres.inventory.Inventory;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 class GameTest {
 
+    @Test
+    void should_create_game_empty() {
+
+        Game game = new Game();
+
+        assertThat(game).isNotNull();
+    }
+    @Test
+    void should_create_game_with_goal_amount() {
+        CashFlow cashFlow = new CashFlow(50.00);
+        Game game = new Game(cashFlow);
+
+        assertThat(game.getGoalAmount().getValue()).isEqualTo(50.00);
+    }
 }
