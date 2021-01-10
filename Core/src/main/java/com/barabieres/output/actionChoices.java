478d4301b59e86297f00b3246d367ecd9869bc1b
PatateHunter <ichtitski@gmail.com>
2021-01-10 22:13:22 +0100

    add Ichai Output display StartMenu, rules and ActionMenus.

---
+++ Core/src/main/java/com/barabieres/output/actionChoices.java
@@ -0,0 +1,20 @@
+package com.barabieres.output;
+
+public enum actionChoices {
+    buy("Buy beers"),
+    sellPrices("Modify selling prices"),
+    hard("Show Inventory"),
+    display("Start Selling"),
+    give_up("Give up");
+
+    private String name;
+    actionChoices(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override public String toString() { return getName(); }
+}
---
+++ Core/src/main/java/com/barabieres/output/startChoices.java
@@ -0,0 +1,20 @@
+package com.barabieres.output;
+
+public enum startChoices {
+easy_mode("Easy mode"),
+    normal("Normal mode"),
+    hard("Hard mode"),
+    display("Display Rules");
+
+    private String name;
+    startChoices(String name){
+     this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override public String toString() { return getName(); }
+
+}
